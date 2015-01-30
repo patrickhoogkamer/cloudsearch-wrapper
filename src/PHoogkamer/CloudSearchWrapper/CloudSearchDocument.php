057ece3b28a3410d4b0bcd174d9cede95b784429
Patrick Hoogkamer <patrickhoogkamer@gmail.com>
2015-01-30 08:42:45 +0100

    Added push functionality.

---
+++ src/PHoogkamer/CloudSearchWrapper/CloudSearchDocument.php
@@ -0,0 +1,59 @@
+<?php namespace PHoogkamer\CloudSearchWrapper;
+
+/**
+ * Class CloudSearchDocument
+ */
+class CloudSearchDocument {
+
+	private $id;
+
+	private $fields;
+
+	private $type;
+
+	//TODO magic getter and setter for individual fields
+
+	/**
+	 * @param $id
+	 */
+	public function __construct($id)
+	{
+		$this->id = $id;
+	}
+
+	/**
+	 * @param array $fields
+	 * @param bool  $filterNullFields
+	 */
+	public function setFields(array $fields, $filterNullFields = true)
+	{
+		if($filterNullFields)
+		{
+			$fields = array_filter($fields);
+		}
+
+		$this->fields = $fields;
+	}
+
+	public function setTypeAdd()
+	{
+		$this->type = 'add';
+	}
+
+	public function setTypeDelete()
+	{
+		$this->type = 'delete';
+	}
+
+	/**
+	 * @return array
+	 */
+	public function getDocument()
+	{
+		return [
+			'type'		=> 'add',
+			'id'		=> $this->id,
+			'fields'	=> $this->fields
+		];
+	}
+}

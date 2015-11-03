187ac80e049aabb4b5f822bf82bd43b20ea67718
Patrick Hoogkamer <patrickhoogkamer@gmail.com>
2015-11-03 09:19:16 +0100

    Refactor and make it possible to loop over hits
    Can now loop over hits with a closure (receiving each hit as a parameter)

--- src/PHoogkamer/CloudSearchWrapper/CloudSearchClient.php
+++ src/PHoogkamer/CloudSearchWrapper/CloudSearchClient.php
@@ -3,6 +3,7 @@
 namespace PHoogkamer\CloudSearchWrapper;
 
 use Aws\CloudSearchDomain\CloudSearchDomainClient;
+use Closure;
 
 /**
  * Class CloudSearchClient
@@ -84,29 +85,59 @@ class CloudSearchClient
         CloudSearchStructuredQuery $filterQuery = null,
         $resultDocument = '\PHoogkamer\CloudSearchWrapper\CloudSearchDocument'
     ) {
-        $args = [
-            'queryParser' => $query->getQueryParserType(),
-            'query'       => $query->getQuery(),
-            'start'       => $query->getStart(),
-            'size'        => $query->getSize(),
-            'sort'        => $query->getSort()
-        ];
+        $arguments = $this->prepareArguments($query, $filterQuery);
 
-        $facet = $query->getFacet();
-
-        if ( ! $query->facetIsEmpty()) {
-            $args['facet'] = $facet;
-        }
-
-        if ( ! is_null($filterQuery)) {
-            $args['filterQuery'] = $filterQuery->getQuery();
-        }
-
-        $args = array_filter($args);
-
-        $result = $this->convertResult($this->searchClient->search($args), $resultDocument);
+        $result = $this->convertResult($this->searchClient->search($arguments), $resultDocument);
 
         return $result;
+    }
+
+    /**
+     * Iterates over results from a query. If $shouldIterate is true then will page until there are no results left.
+     * Even able to resume where left of (if same $query object is used), for example when closure returns false.
+     *
+     * @param Closure                         $closure
+     * @param CloudSearchQueryInterface       $query
+     * @param CloudSearchStructuredQuery|null $filterQuery
+     * @param bool|false                      $shouldIterate
+     * @param string                          $resultDocument
+     */
+    public function loop(
+        Closure $closure,
+        CloudSearchQueryInterface $query,
+        CloudSearchStructuredQuery $filterQuery = null,
+        $shouldIterate = false,
+        $resultDocument = '\PHoogkamer\CloudSearchWrapper\CloudSearchDocument'
+    ) {
+        if($shouldIterate) {
+            $query->setStart(null);
+
+            if(is_null($query->getCursor())) {
+                $query->useCursor();
+
+                $cursor = 'initial';
+            }
+
+            do {
+                $result = $this->searchWithCursor($query, $filterQuery, $resultDocument, $cursor);
+
+                if($result->getNextCursor() != $cursor) {
+                    $cursor = $result->getNextCursor();
+                } else {
+                    $cursor = false;
+                }
+
+                $continueLooping = $this->loopOverHits($closure, $result);
+
+                if(isset($continueLooping) && $continueLooping === false) {
+                    return;
+                }
+            } while($cursor);
+        } else {
+            $result = $this->search($query, $filterQuery, $resultDocument);
+
+            $this->loopOverHits($closure, $result);
+        }
     }
 
     /**
@@ -120,9 +151,10 @@ class CloudSearchClient
         $time         = $awsResult->getPath('status/timems');
         $amountOfHits = $awsResult->getPath('hits/found');
         $start        = $awsResult->getPath('hits/start');
+        $cursor       = $awsResult->getPath('hits/cursor');
         $facets       = $awsResult->getPath('facets');
 
-        $result = new CloudSearchResult($amountOfHits, $start, $time, $facets);
+        $result = new CloudSearchResult($amountOfHits, $start, $time, $facets, $cursor);
 
         $result->fillWithHits($awsResult->getPath('hits/hit'), $resultDocument);
 
@@ -174,5 +206,76 @@ class CloudSearchClient
         ];
 
         $this->pushClient->uploadDocuments($args);
+    }
+
+    /**
+     * @param CloudSearchQueryInterface  $query
+     * @param CloudSearchStructuredQuery $filterQuery
+     * @return array
+     */
+    private function prepareArguments(CloudSearchQueryInterface $query, CloudSearchStructuredQuery $filterQuery = null)
+    {
+        $arguments = [
+            'queryParser' => $query->getQueryParserType(),
+            'query'       => $query->getQuery(),
+            'start'       => $query->getStart(),
+            'size'        => $query->getSize(),
+            'sort'        => $query->getSort()
+        ];
+
+        $facet = $query->getFacet();
+
+        if ( ! $query->facetIsEmpty()) {
+            $arguments['facet'] = $facet;
+        }
+
+        if ( ! is_null($filterQuery)) {
+            $arguments['filterQuery'] = $filterQuery->getQuery();
+        }
+
+        $cursor = $query->getCursor();
+        if ( ! is_null($cursor)) {
+            $arguments['cursor'] = $cursor;
+        }
+
+        $arguments = array_filter($arguments);
+
+        return $arguments;
+    }
+
+    /**
+     * @param Closure $closure
+     * @param         $result
+     * @return mixed
+     */
+    private function loopOverHits(Closure $closure, $result)
+    {
+        foreach ($result->getHits() as $hit) {
+            //If return is false: continue current set, then stop
+            $continueLooping = $closure($hit);
+        }
+
+        return $continueLooping;
+    }
+
+    /**
+     * @param CloudSearchQueryInterface $query
+     * @param CloudSearchStructuredQuery $filterQuery
+     * @param                            $resultDocument
+     * @param                            $cursor
+     * @return CloudSearchResult
+     */
+    private function searchWithCursor(
+        CloudSearchQueryInterface $query,
+        CloudSearchStructuredQuery $filterQuery,
+        $resultDocument,
+        $cursor
+    ) {
+        if ($cursor != 'initial') {
+            $query->setCursor($cursor);
+        }
+        $result = $this->search($query, $filterQuery, $resultDocument);
+
+        return $result;
     }
 }
